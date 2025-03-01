09d363cbdfd7d4d470a4bddfca8f3f53e9ae3fd0
mreed <78713317+mreed4@users.noreply.github.com>
2025-03-01 13:54:21 -0800

    Move CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { PlaygroundProvider } from "./PlaygroundContext";
 import PlaygroundList from "./PlaygroundList";
 import PlaygroundPage from "./PlaygroundPage";
 import NotFoundPage from "./NotFoundPage"; // Import the NotFoundPage component
-import ShortcutMessage from "./ShortcutMessage"; // Import the ShortcutMessage component
 
 const App = () => {
   return (
     <Router>
       <PlaygroundProvider>
         <Routes>
-          <Route path="/" element={<Navigate to="/playgrounds" />} /> {/* Redirect from "/" to "/playgrounds" */}
-          <Route path="/playgrounds" element={<PlaygroundList />} />
+          <Route path="/" element={<PlaygroundList />} />
           <Route path="/playgrounds/:city/:id" element={<PlaygroundPage />} />
           <Route path="*" element={<NotFoundPage />} /> {/* Add 404 route */}
         </Routes>
       </PlaygroundProvider>
-      <ShortcutMessage />
     </Router>
   );
 };
